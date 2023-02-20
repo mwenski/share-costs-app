aeae555b1c3b4f7675d2bb56bb91d8ffbaa48bf7
mwenski <mwenski@outlook.com>
2023-02-20 18:52:57 +0100

    Firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'routes.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MaterialApp(
     title: "Share Cost App",
-    initialRoute: "/new_group",
+    initialRoute: "/",
     routes: Routes.routes,
+
   ));
+
 }
+
