1bc2db3c5e3d50b70ec1996e0947550d65df0e0f
mwenski <mwenski@outlook.com>
2023-02-27 17:38:30 +0100

    removeExpense() created

--- lib/components/expenses_list_item.dart
+++ lib/components/expenses_list_item.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ExpensesListItem extends StatelessWidget {
   const ExpensesListItem(
       {super.key,
+      required this.id,
       required this.name,
       required this.amount,
       required this.date,
       required this.addedBy});
 
+  final String id;
   final String name;
   final double amount;
   final String date;
@@ -15,6 +20,15 @@ class ExpensesListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    void removeExpense(){
+      FirebaseFirestore.instance
+          .collection("expenses")
+          .doc(id)
+          .delete()
+          .then((_) {print("Expense Removed");});
+    }
+
     return Container(
       margin: const EdgeInsets.all(2.0),
       padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
@@ -42,7 +56,10 @@ class ExpensesListItem extends StatelessWidget {
               Text(date,
                   style: const TextStyle(fontSize: 20, color: Colors.grey))
             ],
-          )
+          ),
+          Row(children: [
+            TextButton(onPressed: removeExpense, child: const Text("Remove expense"))
+          ],)
         ],
       ),
     );
