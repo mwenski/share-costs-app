7884ff32e8b4186a01d6ccdcaa22e80ca59b765a
mwenski <mwenski@outlook.com>
2023-03-27 19:55:01 +0200

    firebase_auth added

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:share_cost_app/components/expenses_tab.dart';
 import 'package:share_cost_app/components/balance_list_item.dart';
 import 'package:share_cost_app/routes.dart';
